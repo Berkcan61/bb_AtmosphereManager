248cde98295d475051374aee9e7b956f3763eb75
Berkcan61 <91127805+Berkcan61@users.noreply.github.com>
2024-07-01 15:32:09 +0200

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+-- Hier kann der Spieler die bevorzugte Sprache einstellen
+Config.Locale = 'en'  -- Mögliche Werte: 'de', 'en'
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'berkcan61' -- If you have any questions about the script or tips for improvements, feel free to message me on Discord. I typically respond within two days.
+description 'A comprehensive script for managing weather, time, and ambient sounds in FiveM.'
+version '1.0'
+
+--files {
+   -- 'locales/en.json',
+   -- 'locales/de.json',
+   -- 'config/config.lua'
+--}
+
+server_scripts {
+    'server.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
